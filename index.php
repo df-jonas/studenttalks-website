4dcbdeb7618506a120156073da3dbe9600387328
Jonas <defrere.jonas@gmail.com>
2018-04-24 17:07:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Student Talks 2017</title>
+    <meta charset="utf-8"/>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!--[if lte IE 8]>
+    <script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css"/>
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" href="assets/css/ie9.css"/><![endif]-->
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="assets/css/ie8.css"/><![endif]-->
+</head>
+<body>
+
+<!-- Sidebar -->
+<section id="sidebar">
+    <div class="inner">
+        <nav>
+            <ul>
+                <li><a href="#start">Start</a></li>
+                <li><a href="#programma">Sprekers</a></li>
+                <li><a href="#contact">Reserveren</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
+
+
+<!-- Wrapper -->
+<div id="wrapper">
+    <!-- Intro -->
+    <section id="start" class="wrapper style1 fullscreen fade-up">
+        <div class="inner">
+            <h1>Student Talks 2017</h1>
+            <p>Heb je je al ooit afgevraagd: Wat is dat nu precies, de cloud? Of heb je er genoeg van om je technofiele
+                vrienden te horen spreken over bits en bytes en geen idee te hebben wat ze bedoelen?</p>
+
+            <p>Dan is de eerste editie van StudentTalks precies waar je moet zijn! Als studenten Toegepaste Informatica
+                aan de Erasmushogeschool Brussel geven wij jullie een avond lang uitleg over de nieuwste technologieën
+                van de laatste jaren! Kom mee luisteren hoe Artificiële Intelligentie te werk gaat en welke impact deze
+                kan hebben op onze toekomst! Of denk mee over de vraag of 'slimme' koelkasten en auto's wel een goed
+                idee zijn!</p>
+
+            <p>Reserveer snel je plaats en zorg dat je erbij bent op 6 december 2017 voor deze leerrijke avond!</p>
+            <ul class="actions">
+                <li><a href="#programma" class="button scrolly">Bekijk onze sprekers!</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- One -->
+    <div id="programma" class="wrapper">
+        <div class="inner">
+            <h1>Sprekers</h1>
+            <section style="clear: both;">
+                <h2>Maxime Walravens</h2>
+                <span class="image left"><img src="images/maxime.png" alt=""/></span>
+                <p><b>Alexa en Artificiële Intelligentie</b></p>
+                <p>Een rondleinding in de wonderlijke wereld van Artificial Intelligence. Jullie krijgen een
+                    algemene inleiding, maar ook een diepere duik in praktische voorbeelden!</p>
+            </section>
+            <section style="clear: both;">
+                <h2>Illya Kuys</h2>
+                <span class="image left"><img src="images/illya.png" alt=""/></span>
+                <p><b>Internet of Things & Privacy</b></p>
+                <p>In een maatschappij waar technologie sneller evolueert dan ooit tevoren, waar speelgoed en koelkasten
+                    met elkaar kunnen praten via het internet, zijn toch enkele cruciale vragen die gesteld moeten
+                    worden en kritische meningen die geuit moeten worden. Illya Kuys zal bespreken wat de Internet of
+                    Things is, de voor en nadelen van deze hyperconnectiviteit en welk effect dit heeft op onze
+                    privacy.</p>
+            </section>
+            <section style="clear: both;">
+                <h2>Jonas De Frère</h2>
+                <span class="image left"><img src="images/jonas.png" alt=""/></span>
+                <p><b>Hacking with Word(s)</b></p>
+                <p>Ooit gehoord van een 'hack by Word'? Jonas de Frère zal ons een korte introductie en demonstratie
+                    geven over hoe kwetsbaar jij precies bent, en hoe Microsoft Word gebruikt kan worden door hackers met kwaad
+                    opzet.</p>
+            </section>
+            <section style="clear: both;">
+                <h2>Bernd Wethmar</h2>
+                <span class="image left"><img src="images/bernd.png" alt=""/></span>
+                <p><b>De cloud, wat is dat?</b></p>
+                <p>Van alle technische terminologie kan men zich toch wel het minste inbeelden bij "de Cloud". Om een
+                    beetje verduidelijking te geven wat er precies bedoeld wordt met de cloud, zal Bernd jullie enkele
+                    voorbeelden tonen en de dagelijkse toepassingen bespreken die velen van jullie eigelijk al
+                    gebruiken!</p>
+            </section>
+            <section style="clear: both;">
+                <h2>Bruno Ryckaert</h2>
+                <span class="image left"><img src="images/bruno.png" alt=""/></span>
+                <p><b>Cryptocurrencies</b></p>
+                <p>Bitcoin, Litecoin, Ethereum, Dogecoin, ... Al deze termen betekenden een kleine 10 jaar geleden
+                    niets, maar de dag van vandaag worden deze digitale munteenheden gezien als een potentiële
+                    bedreiging voor onze economie! Hoe deze cryptocurrencies werken en waarom deze zo populair zijn
+                    geworden zal je alles horen van Bruno Ryckaert.</p>
+            </section>
+            <section style="clear: both; margin-bottom: 100px;">
+                <h2>Mitchell Verbruggen</h2>
+                <span class="image left"><img src="images/mitchell.png" alt=""/></span>
+                <p><b>VoIP</b></p>
+                <p>Een kleine 44 jaar geleden heeft de opkomst van de mobiele telefoon onze manier van communicatie
+                    gerevolutioneerd, maar de dag van vandaag staan wij verder dan ooit in onze manier van communicatie.
+                    Voice over IP, een algemene term voor het telefoneren over het internet, is een fenomeen geworden
+                    dat niet meer weg te denken is! Mitchell Verbruggen zal ons meer vertellen over wat VoIP is, wat de
+                    verschillen zijn met klassieke telefonie en wat de toekomst ons brengt.</p>
+            </section>
+        </div>
+    </div>
+
+
+    <section style="clear: both;" id="contact" class="wrapper style1 fade-up">
+        <div class="inner">
+            <h2>Reserveren</h2>
+            <p>Reserveer hier je plaats voor Student Talks op 6 december 2017. Momenteel voorzien wij 1 plaats per
+                persoon. Om te registreren voer je alle invulvelden correct in en
+                druk je op versturen.</p>
+            <div class="split style1">
+                <section>
+                    <p>De reservaties zijn helaas volgelopen. Meer succes volgend jaar!</p>
+                </section>
+                <section>
+                    <ul class="contact">
+                        <li>
+                            <h3>Datum</h3>
+                            <span>6 december 2017</span><br>
+                            <span>Opening om 17:45</span>
+                        </li>
+                        <li>
+                            <h3>Adres</h3>
+                            <a href="https://www.google.be/maps/place/Zespenningenstraat+70,+1000+Brussel/@50.8475229,4.3413367,17z/data=!4m5!3m4!1s0x47c3c477cfb60cb7:0x8f70a08cfae889e9!8m2!3d50.8475229!4d4.3435254">
+                                Erasmushogeschool<br>Campus Bloemenhof (MMM)<br><br>Zespenningenstraat 70<br>1000
+                                Brussel</a>
+                        </li>
+                        <li>
+                            <h3>Bereikbaarheid</h3>
+                            <span>Metro 2/6 : Sint-Katelijne</span><br>
+                            <span>Tram: Anneessens</span><br>
+                            <a href="https://www.erasmushogeschool.be/sites/default/files/Bereikbaarheidsfiche%20Campus%20BLOEMENHOF.pdf">Meer
+                                info</a>
+                        </li>
+                        <li>
+                            <h3>Social</h3>
+                            <ul class="icons">
+                                <li><a href="https://www.facebook.com/events/721469038038694" class="fa-facebook"><span
+                                                class="label">Facebook</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+<!-- Footer -->
+<footer id="footer" class="wrapper style1-alt">
+    <div class="inner">
+        <ul class="menu">
+            <li><img style="height: 50px;" src="images/ehb.png"></li>
+            <li>Copyright &copy <?php echo date('Y'); ?> - Student Talks - Alle rechten voorbehouden.</li>
+            <li>Design by <a href="https://www.defrere.be">Jonas De Frère</a> en <a href="http://bernd.wethmar.be">Bernd
+                    Wethmar</a></li>
+        </ul>
+    </div>
+</footer>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]>
+<script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
---
+++ privacy.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Generic - Hyperspace by HTML5 UP</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!--[if lte IE 8]>
+    <script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css"/>
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" href="assets/css/ie9.css"/><![endif]-->
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="assets/css/ie8.css"/><![endif]-->
+</head>
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Main -->
+    <section id="main" class="wrapper">
+        <div class="inner">
+            <h1 class="major">Privacybeleid</h1>
+            <p>Op studenttalks.be verzamelen wij de volgende gegevens bij het maken van een reservatie: Naam, Email
+                adres, IP adres.</p>
+            <p>Dit is waarom wij deze verzamelen:<br>
+                <b>Naam:</b> Dit gegeven wordt enkel gebruikt om een gastenlijst bij te houden, voor gebruik op de dag
+                van het
+                evenement.<br>
+                <b>Email adres:</b> Een week voor aanvang van het evenement zal er een herinneringsmail verstuurd
+                worden. Enkel hiervoor wordt dit gegeven gebruikt.<br>
+                <b>IP adres:</b> Om intern te controleren dat er geen valse reservaties zijn gemaakt wordt per
+                reservatie het IP adres bijgehouden vanaf waar de reservatie gemaakt is.<br>
+                Al deze gegevens zijn enkel voor intern gebruik bedoeld en worden nooit gedeeld met derden. Na afloop
+                van het evenement worden alle verzamelde gegevens verwijderd.<br>
+                Indien u wenst dat deze gegevens gecorrigeerd of verwijderd worden, gelieve een mail te sturen naar
+                <a href="mailto:bernd@wethmar.be">bernd@wethmar.be</a>. Het verwijderen van uw gegevens zal resulteren
+                in het schrappen van uw reservatie.</p>
+            <p>Tijdens het evenement is het mogelijk dat er foto’s gemaakt worden. Deze kunnen gebruikt worden als
+                promotiemateriaal voor volgende evenementen. Als u hier verder vragen over hebt of een foto niet
+                gepubliceerd wilt hebben, gelieve een mail te sturen naar <a href="mailto:bernd@wethmar.be">bernd@wethmar.be</a>.
+            </p>
+        </div>
+    </section>
+
+</div>
+
+<!-- Footer -->
+<footer id="footer" class="wrapper alt">
+    <div class="inner">
+        <ul class="menu">
+            <li>Copyright &copy <?php echo date('Y'); ?> - Student Talks - Alle rechten voorbehouden.</li>
+            <li>Design by <a href="https://www.defrere.be">Jonas De Frère</a> en <a href="http://bernd.wethmar.be">Bernd
+                Wethmar</a></li>
+        </ul>
+    </div>
+</footer>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]>
+<script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
